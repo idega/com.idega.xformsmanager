f705e6ad4488974347f8984d7e76b437295b7eef
Vytautas Civilis <civilis@idega.com>
2008-11-13 20:12:27 +0000

    isSubmitButton removed

--- src/java/com/idega/xformsmanager/manager/XFormsManagerButton.java
+++ src/java/com/idega/xformsmanager/manager/XFormsManagerButton.java
@@ -5,17 +5,17 @@ import com.idega.xformsmanager.component.FormComponentPage;
 
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.1 $
- *
- * Last modified: $Date: 2008/10/27 10:27:35 $ by $Author: civilis $
+ * @version $Revision: 1.2 $
+ * 
+ *          Last modified: $Date: 2008/11/13 20:12:27 $ by $Author: civilis $
  */
 public interface XFormsManagerButton extends XFormsManager {
-	
-	public abstract void renewButtonPageContextPages(FormComponent component, FormComponentPage previous, FormComponentPage next);
-	
-	public abstract void setLastPageToSubmitButton(FormComponent component, String last_page_id);
-	
+
+	public abstract void renewButtonPageContextPages(FormComponent component,
+			FormComponentPage previous, FormComponentPage next);
+
+	public abstract void setLastPageToSubmitButton(FormComponent component,
+			String last_page_id);
+
 	public abstract String getReferAction(FormComponent component);
-	
-	public abstract boolean isSubmitButton(FormComponent component);
 }
