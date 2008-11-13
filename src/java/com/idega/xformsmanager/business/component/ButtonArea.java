e3cb0af891ad4c04a121ad0f869e5bf3e9569cf6
Vytautas Civilis <civilis@idega.com>
2008-11-13 20:06:33 +0000

    getButton by type removed

--- src/java/com/idega/xformsmanager/business/component/ButtonArea.java
+++ src/java/com/idega/xformsmanager/business/component/ButtonArea.java
@@ -2,13 +2,11 @@ package com.idega.xformsmanager.business.component;
 
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  * 
- *          Last modified: $Date: 2008/10/31 18:30:43 $ by $Author: civilis $
+ *          Last modified: $Date: 2008/11/13 20:06:33 $ by $Author: civilis $
  */
 public interface ButtonArea extends Container {
-
-	public abstract Button getButton(ConstButtonType button_type);
 
 	public abstract Button addButton(ConstButtonType button_type,
 			String component_after_this_id) throws NullPointerException;
