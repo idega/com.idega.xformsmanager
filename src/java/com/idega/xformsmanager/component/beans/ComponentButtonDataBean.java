a9d0a57c8eb21ecee9c3629f05caccfb1ae9e0c9
Vytautas Civilis <civilis@idega.com>
2008-11-13 20:09:55 +0000

    cleanup

--- src/java/com/idega/xformsmanager/component/beans/ComponentButtonDataBean.java
+++ src/java/com/idega/xformsmanager/component/beans/ComponentButtonDataBean.java
@@ -4,9 +4,9 @@ import org.w3c.dom.Element;
 
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  * 
- *          Last modified: $Date: 2008/11/06 14:15:48 $ by $Author: civilis $
+ *          Last modified: $Date: 2008/11/13 20:09:55 $ by $Author: civilis $
  */
 public class ComponentButtonDataBean extends ComponentDataBean {
 
@@ -19,27 +19,6 @@ public class ComponentButtonDataBean extends ComponentDataBean {
 	public void setToggleElement(Element toggle_element) {
 		this.toggle_element = toggle_element;
 	}
-
-	/*
-	@Override
-	public Object clone() {
-
-		ComponentButtonDataBean clone = (ComponentButtonDataBean) super.clone();
-
-		// try {
-		// clone = (ComponentButtonDataBean)super.clone();
-		//			
-		// } catch (Exception e) {
-		//			
-		// clone = new ComponentButtonDataBean();
-		// }
-
-		if (toggle_element != null)
-			clone.setToggleElement((Element) toggle_element.cloneNode(true));
-
-		return clone;
-	}
-	*/
 
 	@Override
 	protected ComponentDataBean getDataBeanInstance() {
