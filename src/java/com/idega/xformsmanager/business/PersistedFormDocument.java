54e0543451aa677f6fcf3065dd666895d4416126
Vytautas Civilis <civilis@idega.com>
2008-12-28 11:53:45 +0000

    version property added

--- src/java/com/idega/xformsmanager/business/PersistedFormDocument.java
+++ src/java/com/idega/xformsmanager/business/PersistedFormDocument.java
@@ -2,52 +2,48 @@ package com.idega.xformsmanager.business;
 
 import org.w3c.dom.Document;
 
-
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.1 $
- *
- * Last modified: $Date: 2008/10/27 10:27:34 $ by $Author: civilis $
+ * @version $Revision: 1.2 $
+ * 
+ *          Last modified: $Date: 2008/12/28 11:53:45 $ by $Author: civilis $
  */
 public class PersistedFormDocument {
-	
+
 	private Document xformsDocument;
 	private Long formId;
 	private String formType;
-	
+	private Integer version;
+
 	public Document getXformsDocument() {
 		return xformsDocument;
 	}
+
 	public void setXformsDocument(Document xformsDocument) {
 		this.xformsDocument = xformsDocument;
 	}
+
 	public Long getFormId() {
 		return formId;
 	}
+
 	public void setFormId(Long formId) {
 		this.formId = formId;
 	}
+
 	public String getFormType() {
 		return formType;
 	}
+
 	public void setFormType(String formType) {
 		this.formType = formType;
 	}
-	/*
-	public void setLocale(Locale locale) {
-		Document doc = getXformsDocument();
-		
-		//ComponentsGenerator componentsGenerator = ComponentsGeneratorImpl.getInstance();
-		FormManagerUtil.modifyXFormsDocumentForViewing(doc);
-		FormManagerUtil.setCurrentFormLocale(doc, locale);
-		setXformsDocument(doc);
-			
-//		componentsGenerator.setDocument(doc);
-//		try {
-//			doc = componentsGenerator.generateHtmlComponentsDocument();
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
+
+	public Integer getVersion() {
+		return version;
 	}
-	*/
+
+	public void setVersion(Integer version) {
+		this.version = version;
+	}
 }
