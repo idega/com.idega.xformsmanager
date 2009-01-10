0dddf88e9cb0f2ff610b3c7631947f1f28459db2
Vytautas Civilis <civilis@idega.com>
2009-01-10 12:33:27 +0000

    openFormLazy method added, javadocs added

--- src/java/com/idega/xformsmanager/business/DocumentManager.java
+++ src/java/com/idega/xformsmanager/business/DocumentManager.java
@@ -15,45 +15,87 @@ import com.idega.xformsmanager.util.InitializationException;
 
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.1 $
- *
- * Last modified: $Date: 2008/10/27 10:27:34 $ by $Author: civilis $
+ * @version $Revision: 1.2 $
+ * 
+ *          Last modified: $Date: 2009/01/10 12:33:27 $ by $Author: civilis $
  */
 public interface DocumentManager {
 
-	public abstract com.idega.xformsmanager.business.Document createForm(LocalizedStringBean formName, String formType);
+	public abstract com.idega.xformsmanager.business.Document createForm(
+			LocalizedStringBean formName, String formType);
 
 	/**
 	 * 
 	 * @return List of available form components types by category
 	 */
-	public abstract List<String> getAvailableFormComponentsTypesList(ConstComponentCategory category);
-	
+	public abstract List<String> getAvailableFormComponentsTypesList(
+			ConstComponentCategory category);
+
 	/**
 	 * 
 	 * @return List of available form components types by datatype
 	 */
-	public abstract List<ComponentType> getComponentsByDatatype(ConstComponentDatatype category);
+	public abstract List<ComponentType> getComponentsByDatatype(
+			ConstComponentDatatype category);
 
-	public abstract com.idega.xformsmanager.business.Document openForm(Long formId);
-	
-	public abstract com.idega.xformsmanager.business.Document openForm(Document xformsDoc);
-	
-	public abstract com.idega.xformsmanager.business.Document takeForm(Long formIdToTakeFrom);
-	
-	public abstract void setPersistenceManager(PersistenceManager persistence_manager);
-	
-	public abstract void init(IWMainApplication iwma) throws InitializationException;
-	
+	/**
+	 * opens form by id, and loads all the structure. @see openFormLazy for lazy
+	 * loading behavior
+	 * 
+	 * @param formId
+	 * @return
+	 */
+	public abstract com.idega.xformsmanager.business.Document openForm(
+			Long formId);
+
+	/**
+	 * opens form by id, but doesn't load all the structure. Loading is deferred
+	 * to when necessary (lazy loading)
+	 * 
+	 * @param formId
+	 * @return
+	 */
+	public abstract com.idega.xformsmanager.business.Document openFormLazy(
+			Long formId);
+
+	/**
+	 * opens form by xforms xml, and loads all the structure. @see openFormLazy
+	 * for lazy loading behavior
+	 * 
+	 * @param xformsDoc
+	 * @return
+	 */
+	public abstract com.idega.xformsmanager.business.Document openForm(
+			Document xformsDoc);
+
+	/**
+	 * used to fix the version of the form that is in flux state (not fixed
+	 * state). In other words, it stores the copy of the form of id provided
+	 * (formIdToTakeFrom), and generates new form id for it (for static version)
+	 * 
+	 * @param formIdToTakeFrom
+	 *            id of the form to make static version from
+	 * @return document of the fixed version form (static)
+	 */
+	public abstract com.idega.xformsmanager.business.Document takeForm(
+			Long formIdToTakeFrom);
+
+	public abstract void setPersistenceManager(
+			PersistenceManager persistence_manager);
+
+	public abstract void init(IWMainApplication iwma)
+			throws InitializationException;
+
 	public abstract boolean isInited();
-	
+
 	public abstract void setCacheManager(CacheManager cacheManager);
-	
-	public abstract void setTransformerService(TransformerService transformerService);
-	
+
+	public abstract void setTransformerService(
+			TransformerService transformerService);
+
 	public abstract void setComponentsXforms(Document componentsXforms);
-	
+
 	public abstract void setComponentsXsd(Document componentsXsd);
-	
+
 	public abstract void setFormXformsTemplate(Document formXformsTemplate);
 }
