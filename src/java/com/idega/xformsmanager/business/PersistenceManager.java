7e511821cce357078e3d39947ace4018e47949cc
Vytautas Civilis <civilis@idega.com>
2009-01-14 14:19:51 +0000

    saveForm with basePath added

--- src/java/com/idega/xformsmanager/business/PersistenceManager.java
+++ src/java/com/idega/xformsmanager/business/PersistenceManager.java
@@ -11,23 +11,34 @@ import com.idega.xformsmanager.component.FormDocument;
 
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.3 $
- * 
- *          Last modified: $Date: 2008/10/31 18:30:43 $ by $Author: civilis $
+ * @version $Revision: 1.4 $ Last modified: $Date: 2009/01/14 14:19:51 $ by $Author: civilis $
  */
 public interface PersistenceManager {
-
+	
 	public abstract PersistedFormDocument loadForm(Long formId);
-
+	
 	public abstract PersistedFormDocument loadPopulatedForm(Long submissionId);
-
+	
 	public abstract PersistedFormDocument saveForm(FormDocument document)
-			throws IllegalAccessException;
-
+	        throws IllegalAccessException;
+	
+	/**
+	 * stores document under the storeBasePath specified. This should be the relative path to the
+	 * content repository root in use. E.g. if the root is /files then the base path could look like
+	 * this: /bpm/forms/
+	 * 
+	 * @param document
+	 * @param storeBasePath
+	 * @return
+	 * @throws IllegalAccessException
+	 */
+	public abstract PersistedFormDocument saveForm(FormDocument document,
+	        String storeBasePath) throws IllegalAccessException;
+	
 	public abstract PersistedFormDocument takeForm(Long formId);
-
+	
 	public abstract List<Form> getStandaloneForms();
-
+	
 	/**
 	 * @param formId
 	 *            - not null
@@ -39,10 +50,9 @@ public interface PersistenceManager {
 	 * @throws IOException
 	 */
 	public abstract Long saveSubmittedData(Long formId, InputStream is,
-			String identifier, boolean finalSubmission) throws IOException;
-
+	        String identifier, boolean finalSubmission) throws IOException;
+	
 	/**
-	 * 
 	 * @param submissionId
 	 * @param formId
 	 *            - not null
@@ -54,12 +64,12 @@ public interface PersistenceManager {
 	 * @throws IOException
 	 */
 	public abstract Long saveSubmittedDataByExistingSubmission(
-			Long submissionId, Long formId, InputStream is, String identifier)
-			throws IOException;
-
+	        Long submissionId, Long formId, InputStream is, String identifier)
+	        throws IOException;
+	
 	public abstract List<Submission> getAllStandaloneFormsSubmissions();
-
+	
 	public abstract List<Submission> getFormsSubmissions(long formId);
-
+	
 	public abstract Submission getSubmission(long submissionId);
 }
