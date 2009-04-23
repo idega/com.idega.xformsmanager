044b4741aaa6020c0d111adaec9c3cb4f86eb659
Vytautas Civilis <civilis@idega.com>
2009-04-23 14:13:59 +0000

    mappingSiblingChanged added

--- src/java/com/idega/xformsmanager/component/impl/FormComponentContainerImpl.java
+++ src/java/com/idega/xformsmanager/component/impl/FormComponentContainerImpl.java
@@ -22,40 +22,38 @@ import com.idega.xformsmanager.manager.XFormsManagerContainer;
 
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.8 $
- * 
- *          Last modified: $Date: 2008/11/20 16:31:28 $ by $Author: civilis $
+ * @version $Revision: 1.9 $ Last modified: $Date: 2009/04/23 14:13:59 $ by $Author: civilis $
  */
 public class FormComponentContainerImpl extends FormComponentImpl implements
-		FormComponentContainer, Container {
-
+        FormComponentContainer, Container {
+	
 	protected List<String> containedComponentsIdsSequence;
 	protected Map<String, FormComponent> containedComponents;
-
+	
 	public void loadContainerComponents() {
-
+		
 		final List<String[]> componentsTypesAndIds = getXFormsManager()
-				.getContainedComponentsTypesAndIds(this);
+		        .getContainedComponentsTypesAndIds(this);
 		final List<String> componentsIds = getContainedComponentsIds();
-
+		
 		final FormComponentFactory componentsFactory = getFormDocument()
-				.getContext().getFormComponentFactory();
-
+		        .getContext().getFormComponentFactory();
+		
 		for (String[] componentTypeAndId : componentsTypesAndIds) {
-
+			
 			String componentType = componentTypeAndId[0];
 			String componentId = componentTypeAndId[1];
 			FormComponent component = componentsFactory.getFormComponentByType(
-					componentType, false);
+			    componentType, false);
 			component.setFormDocument(getFormDocument());
 			// component.setContext(getContext());
-
+			
 			component.setId(componentId);
 			component.setParent(this);
 			// component.setLoad(true);
 			componentsIds.add(componentId);
 			getContainedComponents().put(componentId, component);
-
+			
 			// component.render();
 			component.load();
 		}
@@ -63,122 +61,122 @@ public class FormComponentContainerImpl extends FormComponentImpl implements
 	
 	@Override
 	public XFormsManagerContainer getXFormsManager() {
-
+		
 		return getFormDocument().getContext().getXformsManagerFactory()
-				.getXformsManagerContainer();
+		        .getXformsManagerContainer();
 	}
-
+	
 	public List<String> getContainedComponentsIds() {
-
+		
 		if (containedComponentsIdsSequence == null)
 			containedComponentsIdsSequence = new LinkedList<String>();
-
+		
 		return containedComponentsIdsSequence;
 	}
-
+	
 	public FormComponent getContainedComponent(String componentId) {
-
+		
 		final FormComponent component;
-
+		
 		if (!getContainedComponents().containsKey(componentId)) {
-
+			
 			component = null;
 			Logger.getLogger(getClass().getName()).log(
-					Level.WARNING,
-					"No component found by id = " + componentId
-							+ " in the contrainer (id=" + getId()
-							+ ", form id = " + getFormDocument().getFormId());
+			    Level.WARNING,
+			    "No component found by id = " + componentId
+			            + " in the contrainer (id=" + getId() + ", form id = "
+			            + getFormDocument().getFormId());
 		} else
 			component = getContainedComponents().get(componentId);
-
+		
 		return component;
 	}
-
+	
 	protected Map<String, FormComponent> getContainedComponents() {
-
+		
 		if (containedComponents == null)
 			containedComponents = new HashMap<String, FormComponent>();
-
+		
 		return containedComponents;
 	}
-
+	
 	public Component addComponent(String componentType, String nextSiblingId) {
 		return (Component) addFormComponent(componentType, nextSiblingId);
 	}
-
+	
 	public FormComponent addFormComponent(String componentType,
-			String nextSiblingId) {
-
+	        String nextSiblingId) {
+		
 		FormComponent component = getFormDocument().getContext()
-				.getFormComponentFactory().getFormComponentByType(
-						componentType, true);
+		        .getFormComponentFactory().getFormComponentByType(
+		            componentType, true);
 		// component.setContext(getContext());
 		// component.setFormDocument(getFormDocument());
-
+		
 		String generatedComponentId = getFormDocument()
-				.generateNewComponentId();
+		        .generateNewComponentId();
 		component.setId(generatedComponentId);
 		component.setParent(this);
 		// component.setLoad(false);
 		// component.render();
 		component.setFormDocument(getFormDocument());
-
+		
 		if (nextSiblingId != null) {
-
+			
 			FormComponent nextSibling = getContainedComponent(nextSiblingId);
-
+			
 			if (nextSibling == null)
 				Logger
-						.getLogger(getClass().getName())
-						.log(
-								Level.WARNING,
-								"The next sibling component not found by id provided = "
-										+ nextSiblingId
-										+ ", appending to the end of children. Container id = "
-										+ getId() + " and form id = "
-										+ getFormDocument().getFormId());
-
+				        .getLogger(getClass().getName())
+				        .log(
+				            Level.WARNING,
+				            "The next sibling component not found by id provided = "
+				                    + nextSiblingId
+				                    + ", appending to the end of children. Container id = "
+				                    + getId() + " and form id = "
+				                    + getFormDocument().getFormId());
+			
 			component.setNextSibling(nextSibling);
 		}
-
+		
 		component.create();
-
+		
 		addComponentToChildren(component, nextSiblingId);
 		
 		return component;
 	}
-
+	
 	protected void addComponentToChildren(FormComponent component,
-			String nextSiblingId) {
-
+	        String nextSiblingId) {
+		
 		getContainedComponents().put(component.getId(), component);
-
+		
 		List<String> childrenIds = getContainedComponentsIds();
-
+		
 		// append component to the end of the list
 		if (nextSiblingId == null) {
-
+			
 			if (!childrenIds.isEmpty())
 				// setting the next sibling for component that goes before the
 				// new component
 				getContainedComponent(childrenIds.get(childrenIds.size() - 1))
-						.setNextSibling(component);
-
+				        .setNextSibling(component);
+			
 			// and append the new component id to the end
 			childrenIds.add(component.getId());
-
+			
 		} else {
-
+			
 			// inserting component before next sibling
 			for (int i = 0; i < childrenIds.size(); i++) {
-
+				
 				if (childrenIds.get(i).equals(nextSiblingId)) {
-
+					
 					// tell the component, that will go before this new
 					// component about itself
-					if(i != 0)
-						getContainedComponent(childrenIds.get(i-1)).setNextSibling(
-							component);
+					if (i != 0)
+						getContainedComponent(childrenIds.get(i - 1))
+						        .setNextSibling(component);
 					
 					// and insert to the children list
 					childrenIds.add(i, component.getId());
@@ -188,57 +186,57 @@ public class FormComponentContainerImpl extends FormComponentImpl implements
 			}
 		}
 	}
-
+	
 	public Component getComponent(String componentId) {
 		return (Component) getContainedComponent(componentId);
 	}
-
+	
 	// public void tellComponentId(String componentId) {
 	// getParent().tellComponentId(componentId);
 	// }
-
+	
 	public void rearrangeComponents() {
-
+		
 		final List<String> containedComponentsIds = getContainedComponentsIds();
 		
 		int size = containedComponentsIds.size();
 		Map<String, FormComponent> containedComponents = getContainedComponents();
-
+		
 		for (int i = size - 1; i >= 0; i--) {
-
+			
 			String componentId = containedComponentsIds.get(i);
-
+			
 			if (containedComponents.containsKey(componentId)) {
-
+				
 				FormComponent comp = containedComponents.get(componentId);
-
+				
 				if (i != size - 1) {
-
+					
 					comp.setNextSiblingRerender(containedComponents
-							.get(containedComponentsIds.get(i + 1)));
+					        .get(containedComponentsIds.get(i + 1)));
 				} else
 					comp.setNextSiblingRerender(null);
-
+				
 			} else
 				throw new NullPointerException(
-						"Component, which id was provided in list was not found. Provided: "
-								+ componentId);
+				        "Component, which id was provided in list was not found. Provided: "
+				                + componentId);
 		}
 	}
-
+	
 	public void componentsOrderChanged() {
 	}
-
+	
 	@Override
 	protected void changeBindNames() {
 	}
-
+	
 	@Override
 	public void load() {
 		super.load();
 		loadContainerComponents();
 	}
-
+	
 	// @Override
 	// public void render() {
 	//		
@@ -248,80 +246,88 @@ public class FormComponentContainerImpl extends FormComponentImpl implements
 	// if(load)
 	// loadContainerComponents();
 	// }
-
+	
 	@Override
 	public Element getHtmlRepresentation(Locale locale) throws Exception {
 		Logger
-				.getLogger(getClass().getName())
-				.log(
-						Level.WARNING,
-						"Called getHtmlRepresentation on container component. Container doesn't have representation. Container id = "
-								+ getId()
-								+ " and form id = "
-								+ getFormDocument().getFormId());
+		        .getLogger(getClass().getName())
+		        .log(
+		            Level.WARNING,
+		            "Called getHtmlRepresentation on container component. Container doesn't have representation. Container id = "
+		                    + getId()
+		                    + " and form id = "
+		                    + getFormDocument().getFormId());
 		return null;
 	}
-
+	
 	@Override
 	public PropertiesComponent getProperties() {
 		return null;
 	}
-
+	
 	public void unregisterComponent(String componentId) {
-
+		
 		getContainedComponents().remove(componentId);
 		getContainedComponentsIds().remove(componentId);
 	}
-
+	
 	/**
 	 * adds children components to the confirmation page
 	 */
 	@Override
 	public void addToConfirmationPage() {
-
+		
 		List<String> ids = getContainedComponentsIds();
-
+		
 		for (int i = ids.size() - 1; i >= 0; i--)
 			getContainedComponents().get(ids.get(i)).addToConfirmationPage();
 	}
-
+	
 	/**
 	 * removes all children components first, then removes itself
 	 */
 	@Override
 	public void remove() {
-
+		
 		List<String> containedComps = new ArrayList<String>(
-				getContainedComponentsIds());
+		        getContainedComponentsIds());
 		for (String cid : containedComps)
 			getComponent(cid).remove();
-
+		
 		super.remove();
 	}
-
+	
 	public FormComponentPage getParentPage() {
-
+		
 		FormComponentContainer parent = getParent();
-
+		
 		FormComponentPage page = null;
-
+		
 		while (page == null) {
-
+			
 			if (parent instanceof FormComponentPage)
 				page = (FormComponentPage) parent;
 			else
 				parent = parent.getParent();
 		}
-
+		
 		return page;
 	}
 	
 	@Override
 	public void dispatchEvent(Event eventType, Object eventContext,
-			FormComponent dispatcher) {
-
+	        FormComponent dispatcher) {
+		
 		for (FormComponent child : getContainedComponents().values()) {
 			child.dispatchEvent(eventType, eventContext, dispatcher);
+		}
+	}
+	
+	@Override
+	public void mappingSiblingChanged(String... relevantMappings) {
+		
+		for (FormComponent component : getContainedComponents().values()) {
+			component.mappingSiblingChanged(relevantMappings);
 		}
 	}
 }
