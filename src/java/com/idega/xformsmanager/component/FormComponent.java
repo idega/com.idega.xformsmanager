e9d59832ae90a5b2dc7358067fc39cbb26bda1fc
Vytautas Civilis <civilis@idega.com>
2009-04-23 14:12:39 +0000

    appendChildNode, mappingSiblingChanged added

--- src/java/com/idega/xformsmanager/component/FormComponent.java
+++ src/java/com/idega/xformsmanager/component/FormComponent.java
@@ -1,5 +1,7 @@
 package com.idega.xformsmanager.component;
 
+import org.w3c.dom.Node;
+
 import com.idega.xformsmanager.business.component.properties.PropertiesComponent;
 import com.idega.xformsmanager.component.beans.ComponentDataBean;
 import com.idega.xformsmanager.component.impl.FormComponentContainerImpl;
@@ -7,98 +9,95 @@ import com.idega.xformsmanager.component.properties.impl.ConstUpdateType;
 import com.idega.xformsmanager.context.Event;
 
 /**
- * this interface represents "internal to the xform document manager" form
- * component representation. Shouldn't be used outside the xform document
- * manager
+ * this interface represents "internal to the xform document manager" form component representation.
+ * Shouldn't be used outside the xform document manager
  * 
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.5 $
- * 
- *          Last modified: $Date: 2008/11/20 16:31:27 $ by $Author: civilis $
+ * @version $Revision: 1.6 $ Last modified: $Date: 2009/04/23 14:12:39 $ by $Author: civilis $
  */
 public interface FormComponent {
-
+	
 	/**
-	 * loads xforms component from components template. This should be called
-	 * BEFORE create().
+	 * loads xforms component from components template. This should be called BEFORE create().
 	 */
 	public abstract void loadFromTemplate();
-
+	
 	/**
-	 * adds templated component to the xform document. This method should be
-	 * called AFTER loadFromTemplate(). The sufficient information should be
-	 * already added to this component (like parent container)
+	 * adds templated component to the xform document. This method should be called AFTER
+	 * loadFromTemplate(). The sufficient information should be already added to this component
+	 * (like parent container)
 	 * 
 	 * @see FormComponentContainerImpl addComponent method for example
 	 */
 	public abstract void create();
-
+	
 	/**
-	 * loads xforms component from the xform document. The sufficient
-	 * information should be already added to this component (like parent
-	 * container)
+	 * loads xforms component from the xform document. The sufficient information should be already
+	 * added to this component (like parent container)
 	 * 
-	 * @see FormComponentContainerImpl loadContainerComponents method for
-	 *      example
+	 * @see FormComponentContainerImpl loadContainerComponents method for example
 	 */
 	public abstract void load();
-
+	
 	// public abstract void render();
-
+	
 	/**
-	 * specifies the component, before which this component should go in the
-	 * xforms document
+	 * specifies the component, before which this component should go in the xforms document
 	 * 
 	 * @param component
 	 *            sibling component
 	 */
 	public abstract void setNextSibling(FormComponent component);
-
+	
 	public abstract FormComponent getNextSibling();
-
+	
 	/**
 	 * moves this component to new location before next sibling specified
 	 * 
 	 * @param nextSibling
 	 */
 	public abstract void setNextSiblingRerender(FormComponent nextSibling);
-
+	
 	public abstract String getId();
-
+	
 	public abstract void setId(String id);
-
+	
 	public abstract void setType(String type);
-
+	
 	public abstract String getType();
-
+	
 	public abstract PropertiesComponent getProperties();
-
+	
 	public abstract void remove();
-
+	
 	// public abstract void setLoad(boolean load);
-
+	
 	public abstract void setParent(FormComponentContainer parent);
-
+	
 	public abstract void setFormDocument(FormDocument form_document);
-
+	
 	/**
 	 * performs adding component to the confirmation page
 	 */
 	public abstract void addToConfirmationPage();
-
+	
 	public abstract FormComponentContainer getParent();
-
+	
 	public abstract void update(ConstUpdateType what);
-
+	
 	public abstract void update(ConstUpdateType what, Object property);
-
+	
 	public abstract ComponentDataBean getComponentDataBean();
-
+	
 	public abstract void setComponentDataBean(
-			ComponentDataBean componentDataBean);
-
+	        ComponentDataBean componentDataBean);
+	
 	public abstract FormDocument getFormDocument();
-
+	
 	public abstract void dispatchEvent(Event event, Object eventContext,
-			FormComponent dispatcher);
+	        FormComponent dispatcher);
+	
+	public abstract Node appendChildNode(Node childNode);
+	
+	public abstract void mappingSiblingChanged(String... relevantMappings);
 }
--- src/java/com/idega/xformsmanager/component/impl/FormComponentImpl.java
+++ src/java/com/idega/xformsmanager/component/impl/FormComponentImpl.java
@@ -3,6 +3,7 @@ package com.idega.xformsmanager.component.impl;
 import java.util.Locale;
 
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
 import com.idega.xformsmanager.business.component.Component;
 import com.idega.xformsmanager.business.component.properties.PropertiesComponent;
@@ -18,288 +19,302 @@ import com.idega.xformsmanager.context.Event;
 import com.idega.xformsmanager.manager.HtmlManager;
 import com.idega.xformsmanager.manager.XFormsManager;
 import com.idega.xformsmanager.xform.Bind;
+import com.idega.xformsmanager.xform.Nodeset;
 
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.15 $
- * 
- *          Last modified: $Date: 2009/01/20 13:10:55 $ by $Author: valdas $
+ * @version $Revision: 1.16 $ Last modified: $Date: 2009/04/23 14:14:20 $ by $Author: civilis $
  */
 public class FormComponentImpl implements FormComponent, Component {
-
+	
 	private String componentId;
 	private FormDocument formDocument;
 	private ComponentDataBean xformsComponentDataBean;
-
+	
 	protected FormComponent nextSibling;
 	protected String type;
 	protected FormComponentContainer parent;
 	protected PropertiesComponent properties;
-
+	
 	public void loadFromTemplate() {
-
+		
 		XFormsManager xformsManager = getXFormsManager();
 		xformsManager.loadComponentFromTemplate(this);
 	}
-
+	
 	public void create() {
-
+		
 		XFormsManager xformsManager = getXFormsManager();
-
+		
 		xformsManager.addComponentToDocument(this);
 		setProperties();
 		changeBindNames();
-
+		
 		FormDocument formDocument = getFormDocument();
 		formDocument.setFormDocumentModified(true);
 		// tellAboutMe();
-
+		
 		if (formDocument.getContext().getFormComponentFactory()
-				.isNormalFormElement(this)) {
-
+		        .isNormalFormElement(this)) {
+			
 			FormComponentPage confirmationPage = formDocument
-					.getFormConfirmationPage();
-
+			        .getFormConfirmationPage();
+			
 			if (confirmationPage != null) {
 				getXFormsManager().loadConfirmationElement(this,
-						confirmationPage);
+				    confirmationPage);
 			}
 		}
 	}
-
+	
 	public void load() {
-
+		
 		XFormsManager xformsManager = getXFormsManager();
-
+		
 		xformsManager.loadComponentFromDocument(this);
-
+		
 		setProperties();
-
+		
 		// those two could be moved to container who's calling this
 		getFormDocument().setFormDocumentModified(true);
 		// tellAboutMe();
-
+		
 		if (getFormDocument().getContext().getFormComponentFactory()
-				.isNormalFormElement(this)) {
-
+		        .isNormalFormElement(this)) {
+			
 			// TODO: perhaps just lazyload
 			getXFormsManager().loadConfirmationElement(this, null);
 		}
 	}
-
+	
 	public void addToConfirmationPage() {
-
+		
 		if (getFormDocument().getContext().getFormComponentFactory()
-				.isNormalFormElement(this)) {
-
+		        .isNormalFormElement(this)) {
+			
 			FormComponentPage confirmationPage = getFormDocument()
-					.getFormConfirmationPage();
-
+			        .getFormConfirmationPage();
+			
 			if (confirmationPage != null) {
 				getXFormsManager().loadConfirmationElement(this,
-						confirmationPage);
+				    confirmationPage);
 			}
 		}
 	}
-
+	
 	protected void setProperties() {
-
+		
 		ComponentProperties properties = (ComponentProperties) getProperties();
-
+		
 		if (properties == null)
 			return;
-
+		
 		properties.setPlainLabel(getXFormsManager().getLocalizedStrings(this));
 		properties.setPlainRequired(getXFormsManager().isRequired(this));
 		properties.setErrorsMessages(getXFormsManager()
-				.getErrorLabelLocalizedStrings(this));
+		        .getErrorLabelLocalizedStrings(this));
 		properties.setPlainAutofillKey(getXFormsManager().getAutofillKey(this));
 		properties.setPlainHelpText(getXFormsManager().getHelpText(this));
 		properties.setPlainVariable(getXFormsManager().getVariable(this));
 	}
-
+	
 	protected void changeBindNames() {
-
+		
 		Bind bind = getComponentDataBean().getBind();
-
+		
 		if (bind != null && !bind.getIsShared()) {
-
+			
 			LocalizedStringBean localizedLabel = getProperties().getLabel();
-
+			
 			String defaultLocaleLabel = localizedLabel
-					.getString(getFormDocument().getDefaultLocale());
-
+			        .getString(getFormDocument().getDefaultLocale());
+			
 			String newBindName = new StringBuffer(defaultLocaleLabel).append(
-					'_').append(getId()).toString();
-
+			    '_').append(getId()).toString();
+			
 			bind.rename(newBindName);
 		}
-
+		
 		getXFormsManager().bindsRenamed(this);
 	}
-
+	
 	public void setNextSibling(FormComponent component) {
-
+		
 		nextSibling = component;
 	}
-
+	
 	public void setNextSiblingRerender(FormComponent nextSibling) {
-
+		
 		FormComponent formerNextSibling = this.nextSibling;
 		this.nextSibling = nextSibling;
-
+		
 		if (nextSibling != null && formerNextSibling != null
-				&& !formerNextSibling.getId().equals(nextSibling.getId())) {
-
+		        && !formerNextSibling.getId().equals(nextSibling.getId())) {
+			
 			getXFormsManager().moveComponent(this, nextSibling.getId());
-
+			
 		} else if (formerNextSibling == null && nextSibling != null) {
-
+			
 			getXFormsManager().moveComponent(this, nextSibling.getId());
-
+			
 		} else if (nextSibling == null && formerNextSibling != null) {
-
+			
 			getXFormsManager().moveComponent(this, null);
 		}
 	}
-
+	
 	public String getId() {
-
+		
 		return componentId;
 	}
-
+	
 	public void setId(String id) {
-
+		
 		componentId = id;
 	}
-
+	
 	public void setType(String type) {
 		this.type = type;
 	}
-
+	
 	public Element getHtmlRepresentation(Locale locale) throws Exception {
-
+		
 		return getHtmlManager().getHtmlRepresentation(this, locale);
 	}
-
+	
 	public PropertiesComponent getProperties() {
-
+		
 		if (properties == null) {
 			ComponentProperties properties = new ComponentProperties();
 			properties.setComponent(this);
 			this.properties = properties;
 		}
-
+		
 		return properties;
 	}
-
+	
 	public XFormsManager getXFormsManager() {
-
+		
 		return getFormDocument().getContext().getXformsManagerFactory()
-				.getXformsManager();
+		        .getXformsManager();
 	}
-
+	
 	protected HtmlManager getHtmlManager() {
-
+		
 		return getFormDocument().getContext().getHtmlManagerFactory()
-				.getHtmlManager();
+		        .getHtmlManager();
 	}
-
+	
 	public String getType() {
 		return type;
 	}
-
+	
 	public void remove() {
-
+		
 		getXFormsManager().removeComponentFromXFormsDocument(this);
 		getFormDocument().setFormDocumentModified(true);
 		getParent().unregisterComponent(getId());
 	}
-
+	
 	public void setParent(FormComponentContainer parent) {
 		this.parent = parent;
 	}
-
+	
 	public FormComponentContainer getParent() {
 		return parent;
 	}
-
+	
 	@Override
 	public String toString() {
-
+		
 		return "\nComponent id: " + getId() + " component class: " + getClass();
 	}
-
+	
 	public FormComponent getNextSibling() {
 		return nextSibling;
 	}
-
+	
 	public void setFormDocument(FormDocument formDocument) {
 		this.formDocument = formDocument;
 	}
-
+	
 	public FormDocument getFormDocument() {
-
+		
 		if (formDocument == null)
 			formDocument = getParent().getFormDocument();
-
+		
 		return formDocument;
 	}
-
+	
 	public void update(ConstUpdateType what) {
-
+		
 		update(what, null);
 	}
-
+	
 	public void update(ConstUpdateType what, Object prop) {
-
+		
 		getXFormsManager().update(this, what, prop);
-
+		
 		switch (what) {
-		case LABEL:
+			case LABEL:
 
-			getHtmlManager().clearHtmlComponents(this);
-			getFormDocument().setFormDocumentModified(true);
-			changeBindNames();
-			break;
-
-		case ERROR_MSG:
-			getHtmlManager().clearHtmlComponents(this);
-			getFormDocument().setFormDocumentModified(true);
-			break;
-
-		case HELP_TEXT:
-			getHtmlManager().clearHtmlComponents(this);
-			getFormDocument().setFormDocumentModified(true);
-			break;
-
-		case CONSTRAINT_REQUIRED:
-			getFormDocument().setFormDocumentModified(true);
-			break;
-
-		case P3P_TYPE:
-			break;
-
-		case AUTOFILL_KEY:
-			break;
-
-		case VARIABLE_NAME:
-			break;
-
-		default:
-			break;
+				getHtmlManager().clearHtmlComponents(this);
+				getFormDocument().setFormDocumentModified(true);
+				changeBindNames();
+				break;
+			
+			case ERROR_MSG:
+				getHtmlManager().clearHtmlComponents(this);
+				getFormDocument().setFormDocumentModified(true);
+				break;
+			
+			case HELP_TEXT:
+				getHtmlManager().clearHtmlComponents(this);
+				getFormDocument().setFormDocumentModified(true);
+				break;
+			
+			case CONSTRAINT_REQUIRED:
+				getFormDocument().setFormDocumentModified(true);
+				break;
+			
+			case P3P_TYPE:
+				break;
+			
+			case AUTOFILL_KEY:
+				break;
+			
+			case VARIABLE_NAME:
+				break;
+			
+			default:
+				break;
 		}
 	}
-
+	
 	public ComponentDataBean getComponentDataBean() {
 		return xformsComponentDataBean;
 	}
-
+	
 	public void setComponentDataBean(ComponentDataBean xformsComponentDataBean) {
 		this.xformsComponentDataBean = xformsComponentDataBean;
 	}
-
+	
 	public void dispatchEvent(Event event, Object eventContext,
-			FormComponent dispatcher) {
+	        FormComponent dispatcher) {
+	}
+	
+	public Node appendChildNode(Node childNode) {
+		
+		return getComponentDataBean().getElement().appendChild(childNode);
+	}
+	
+	public void mappingSiblingChanged(String... relevantMappings) {
+		
+		Nodeset nodeset = getComponentDataBean().getNodeset();
+		
+		if (nodeset != null) {
+			
+			nodeset.reactToMappingSiblingChanged(this, relevantMappings);
+		}
 	}
 }
