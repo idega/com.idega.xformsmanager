aee407949dba16e5fa719e4ac696dc742b89b638
Vytautas Civilis <civilis@idega.com>
2009-04-29 10:48:48 +0000

    new

---
+++ src/java/com/idega/xformsmanager/xform/FormComponentOutputMappingImpl.java
@@ -0,0 +1,22 @@
+package com.idega.xformsmanager.xform;
+
+import com.idega.xformsmanager.component.FormComponent;
+
+/**
+ * represents no behavior for mapping changes
+ * 
+ * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
+ * @version $Revision: 1.1 $ Last modified: $Date: 2009/04/29 10:48:48 $ by $Author: civilis $
+ */
+public class FormComponentOutputMappingImpl implements FormComponentMapping {
+	
+	public FormComponentOutputMappingImpl(FormComponent formComponent,
+	                                      Nodeset nodeset) {
+	}
+	
+	public void setMapping(String mappingExpression) {
+	}
+	
+	public void removeMapping() {
+	}
+}
