e839ca5de211a34ec72559432ee2efdcaa482dc1
Vytautas Civilis <civilis@idega.com>
2009-05-05 14:06:04 +0000

    getSubmissionIdentifier removed

--- src/java/com/idega/xformsmanager/business/Submission.java
+++ src/java/com/idega/xformsmanager/business/Submission.java
@@ -6,26 +6,22 @@ import org.w3c.dom.Document;
 
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.2 $
- * 
- *          Last modified: $Date: 2009/01/26 11:08:39 $ by $Author: valdas $
+ * @version $Revision: 1.3 $ Last modified: $Date: 2009/05/05 14:06:04 $ by $Author: civilis $
  */
 public interface Submission {
-
+	
 	public abstract Long getSubmissionId();
-
-	public abstract String getSubmissionIdentifier();
-
+	
 	public abstract String getSubmissionStorageIdentifier();
-
+	
 	public abstract String getSubmissionStorageType();
-
+	
 	public abstract Date getDateSubmitted();
-
+	
 	public abstract Form getXform();
-
+	
 	public abstract Boolean getIsFinalSubmission();
-
+	
 	public abstract Document getSubmissionDocument();
 	
 	public abstract String getSubmissionUUID();
